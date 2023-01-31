42857a3ecc58c8f00bb2cd1f0c62f6f13ff74eb9
prem kumar bokka <62457454+prembokka1998@users.noreply.github.com>
2023-01-31 11:58:38 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+function option2(){
+    document.getElementById("etxt").style.display="block";
+    document.getElementById("mcode").style.display="none";
+}
+
+    function main() {
+    var letter = [
+                'a', 'b', 'c', 'd', 'e', 'f',
+	             'g', 'h', 'i', 'j', 'k', 'l',
+	             'm', 'n', 'o', 'p', 'q', 'r',
+	             's', 't', 'u', 'v', 'w', 'x',
+	             'y', 'z', '1', '2', '3', '4',
+	             '5', '6', '7', '8', '9', '0'
+    ];
+    var code = [
+                ".-",   "-...", "-.-.", "-..",  ".",
+                "..-.", "--.",  "....", "..",   ".---",
+                "-.-",  ".-..", "--",   "-.",   "---",
+                ".--.", "--.-", ".-.",  "...",  "-",
+                "..-",  "...-", ".--",  "-..-", "-.--",
+                "--..", "|"
+    ];
+    var english = null;
+    var morsecode = null;
+    var input = null;
+    if(input)
+    }
